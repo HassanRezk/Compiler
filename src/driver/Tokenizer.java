7413ff2f61a7df95d0fe35d6ae9c6c0ad9cafd1f
HassanRezk <hassan.rms93@gmail.com>
2016-12-10 21:01:40 +0200

    Add lexical analysis package.

---
+++ src/driver/Tokenizer.java
@@ -0,0 +1,11 @@
+package driver;
+
+/**
+ * Created by Hassan on 12/10/2016.
+ */
+public class Tokenizer {
+
+    public static void main(final String[] args) {
+
+    }
+}
---
+++ src/lexical_analysis/Token.java
@@ -0,0 +1,7 @@
+package lexical_analysis;
+
+/**
+ * Created by Hassan on 12/10/2016.
+ */
+public class Token {
+}
