617676c2198c76a47914b15a275108202d42567e
Hammam94 <gentle_engineer@hotmail.com>
2016-12-11 21:29:42 +0200

    tokenizer first version

--- src/driver/Tokenizer.java
+++ src/driver/Tokenizer.java
@@ -1,11 +1,20 @@
 package driver;
 
+import lexical_analysis.Token;
+
+import java.util.List;
+
 /**
  * Created by Hassan on 12/10/2016.
  */
 public class Tokenizer {
 
     public static void main(final String[] args) {
+        Token t = new Token("start int t; t = 0; char c = 'a'; end");
+        List<Token.tokens> tokens = t.getTokens();
 
+        for(int i = 0 ; i < tokens.size(); i++) {
+            System.out.println(tokens.get(i).text + "\t," + tokens.get(i).tokenTYpe);
+        }
     }
 }
--- src/lexical_analysis/TokenType.java
+++ src/lexical_analysis/TokenType.java
@@ -4,4 +4,12 @@ package lexical_analysis;
  * Created by Hassan on 12/10/2016.
  */
 public enum TokenType {
+    IDENTIFIER,
+    OPERATOR,
+    KEYWORD,
+    INT,
+    BOOLEAN,
+    FLOAT,
+    SEMICOLON,
+    COMMA
 }
