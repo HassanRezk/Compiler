3566064952d873f1a8e79ca222bffcf4c0d09dc6
myriamreda9 <myriam.dimitry94@gmail.com>
2016-12-27 22:52:50 +0200

    parseTreeNode

---
+++ src/parser/ParseTreeNode.java
@@ -0,0 +1,32 @@
+package parser;
+
+import java.util.ArrayList;
+import java.util.List;
+import grammar.GrammarNode;
+
+public class ParseTreeNode {
+
+	private GrammarNode Value ; 
+	private List<ParseTreeNode> childern = new ArrayList<>();
+	
+	public ParseTreeNode(GrammarNode value ) {
+		
+		this.Value= value ;
+
+	}
+	
+
+	public GrammarNode getValue (){
+		return Value ; 
+	}
+	
+	public List <ParseTreeNode> getChildren (){
+		return children ;
+	}
+	
+	public void AddChild(ParseTreeNode child) {
+		children.add(child);
+		
+	}
+
+}
